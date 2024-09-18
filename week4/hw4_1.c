048f7b5b633d7ef7d92574587a99313f27d8f5d0
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-09-18 22:57:05 +0900

    hw week4

--- week4/hw4_1.c
+++ week4/hw4_1.c
@@ -4,25 +4,54 @@ int main()
 {
     int count = 0;
     int menu;
-    int start = 1;
-    int end = 100;
-    int midnum;
+    int num = 50;
+    int half = 25;
 
     while (1)
     {
-        midnum = (start + end) / 2;
-
         count++;
-        printf("How about %d? (try more : 1, try less : 2, right : 0) ", midnum);
+        printf("How about %d? (try more : 1, try less : 2, right : 0) ", num);
         scanf("%d", &menu);
         if (menu == 1)
-            start = midnum + 1;
+            num += half;
         else if (menu == 2)
-            end = midnum - 1;
+            num -= half;
         else
             break;
+        half = (half + 1) / 2;
+        if (num == 99)
+            half = 1;
     }
     printf("%d tried.", count);
 
     return 0;
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int count = 0;
+//     int menu;
+//     int start = 1;
+//     int end = 100;
+//     int midnum;
+
+//     while (1)
+//     {
+//         midnum = (start + end) / 2;
+
+//         count++;
+//         printf("How about %d? (try more : 1, try less : 2, right : 0) ", midnum);
+//         scanf("%d", &menu);
+//         if (menu == 1)
+//             start = midnum + 1;
+//         else if (menu == 2)
+//             end = midnum - 1;
+//         else
+//             break;
+//     }
+//     printf("%d tried.", count);
+
+//     return 0;
+// }
