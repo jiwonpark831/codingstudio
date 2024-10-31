a6e9e12a3bfe573ea27a33ec72fbe1872db6fb72
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-10-31 23:31:34 +0900

    week9

--- week9/9_9.c
+++ week9/9_9.c
@@ -9,20 +9,47 @@ int main()
 
   char line[100]; // 입력받을 문장
   fgets(line, 100, stdin);
-  line[strlen(line) - 1] = '\0';
-  char *p = findLongestWord(line);
+  if (line[strlen(line) - 1] == '\n')
+    line[strlen(line) - 1] = '\0';
+
   // 이곳에 코드를 작성하라
-  char word[10];
+  char *p = findLongestWord(line);
+  char word[100];
   int i = 0;
   while (p[i] != ' ' && p[i] != '\0')
   {
     word[i] = p[i];
     i++;
   }
-  printf("The longest word in \"%s\" is \"%s\", with a length of %d.",line, word, strlen(word));
+  word[i] = '\0';
+
+  printf("The longest word in \"%s\" is \"%s\", ", line, word);
+  printf("with a length of %d.", strlen(word));
   return 0;
 }
 
 char *findLongestWord(char *str)
 {
+  char *max = NULL;
+  char *temp = str;
+  int templen = 0;
+  int maxlen = 0;
+
+  for (int i = 0; i <= strlen(str); i++)
+  {
+    if (str[i] != ' ' && str[i] != '\0')
+      templen++;
+    else
+    {
+
+      if (templen > maxlen)
+      {
+        maxlen = templen;
+        max = temp;
+      }
+      templen = 0;
+      temp = str + i + 1;
+    }
+  }
+  return max;
 }
